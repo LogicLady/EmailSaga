825e71cf87efc59cc69a1d697d950e5637100286
Jessica Cook <jessi.lynn.cook@gmail.com>
2018-06-04 14:22:30 -0500

    Update single_lookup.py

--- cgi-bin/single_lookup.py
+++ cgi-bin/single_lookup.py
@@ -4,23 +4,31 @@ import cgi, os, pandas, sys
 import cgitb; cgitb.enable()
 from intercom.client import Client
 
+# grab email address from form at emailsa.ga/single.html
 form = cgi.FieldStorage()
 email_input = form.getvalue('email')
 
 # intercom API extended access token
-intercom = Client(personal_access_token='dG9rOjQ2MmM2MzlkXzQwNzRfNGVkYl84YjYwXzkwZDVmNzcyMzcyMjoxOjA=')
+intercom = Client(personal_access_token='####################################')
+
+# API call based on email address from form
 intercom.users.create(email=email_input)
 search = intercom.users.find(email=email_input)
+
+# display results inside the iframe
 print("Content-Type: text/html\n\n")
 print("<div class='result' style='font-family: Arial, Helvetica, sans-serif; text-align: center; margin: 0px 0px 50px 0px;'>")
   
+# display avatar image if present in results
 if str(search.avatar.image_url) != 'None':
   print("<a href='" + str(search.avatar.image_url) + "' target='_blank'><img src='" + str(search.avatar.image_url) + "' alt='Avatar' style='width: 25%; border-radius: 5px; display: block; margin-left: auto; margin-right: auto;'></a>")
 
+# display job title if present in results
 if len(search.custom_attributes) > 0:
   attr = str(search.custom_attributes)
   print("<br>Job Title: " + attr[15:len(attr)-2] + "<br><br>")
-  
+
+# for each social media platform, display results (username, name, and URL)
 for profile in search.social_profiles:
   username = str(profile.username)
   name = str(profile.name)
